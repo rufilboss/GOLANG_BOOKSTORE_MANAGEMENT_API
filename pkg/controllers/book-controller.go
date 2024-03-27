3a2532342ef3d7f8e192d2f392d9db2b0a8d5f4c
Ilyas Rufai <rufaiilyas@gmail.com>
2024-03-27 22:21:47 +0100

    update

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,24 +1,38 @@
- package controllers
+package controllers
 
- import(
+import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
 	"strconv"
-	"github.com/akhil/go-bookstore/pkg/utils"
+
 	"github.com/akhil/go-bookstore/pkg/models"
+	"github.com/gorilla/mux"
 
- )
+	"rufilboss/crud_api/GOLANG_BOOKSTORE_MANAGEMENT_API/pkg/models"
+)
 
- var NewBook = models.Book
+var NewBook = models.Book
 
- func GetBook(w http.ResponseWriter, r *http.Request){
+func GetBook(w http.ResponseWriter, r *http.Request) {
 	newBooks := models.GetAllBooks()
-	res, _ :=json.Marshal(newBooks) 
-	w.Header().Set("Content-Type", "application/json")
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
- }
+}
 
- 
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error parsing book")
+	}
+	bookDetails, _ := models.GetBookById
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+} 
+
