599c4814dbcd8134677dca384cc2ce3addbf3878
Ilyas Rufai <rufaiilyas@gmail.com>
2024-03-27 21:58:40 +0100

    update

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,1 +1,24 @@
+ package controllers
+
+ import(
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+	"github.com/akhil/go-bookstore/pkg/utils"
+	"github.com/akhil/go-bookstore/pkg/models"
+
+ )
+
+ var NewBook = models.Book
+
+ func GetBook(w http.ResponseWriter, r *http.Request){
+	newBooks := models.GetAllBooks()
+	res, _ :=json.Marshal(newBooks) 
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+ }
+
  
